88c02ad970dc1e773541fe2777984e67ea122a46
fukku0619 <2323066@asia-u.ac.jp>
2026-01-07 14:46:01 +0900

    Create Squares2x2Icon.tsx

---
+++ src/components/icons/Squares2x2Icon.tsx
@@ -0,0 +1,7 @@
+export default function Squares2x2Icon() {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" className="w-5 h-5" stroke="currentColor" strokeWidth="2">
+      <path d="M4 4h7v7H4zM13 4h7v7h-7zM4 13h7v7H4zM13 13h7v7h-7z" />
+    </svg>
+  );
+}
