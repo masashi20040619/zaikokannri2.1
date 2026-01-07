34a4e69548d0e154f3243840049a8818971bc5e6
fukku0619 <2323066@asia-u.ac.jp>
2026-01-07 14:46:38 +0900

    Add PencilIcon component with SVG rendering

---
+++ src/components/icons/PencilIcon.tsx
@@ -0,0 +1,8 @@
+export default function PencilIcon() {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" className="w-5 h-5" stroke="currentColor" strokeWidth="2">
+      <path d="M12 20h9" />
+      <path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z" />
+    </svg>
+  );
+}
