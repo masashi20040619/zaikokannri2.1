6ae56e39c52a34437049eb79a3d6bcbc9232be05
fukku0619 <2323066@asia-u.ac.jp>
2026-01-07 14:45:06 +0900

    Add PlusIcon component for SVG icon

---
+++ src/components/icons/PlusIcon.tsx
@@ -0,0 +1,7 @@
+export default function PlusIcon() {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" className="w-5 h-5" stroke="currentColor" strokeWidth="2">
+      <path d="M12 5v14M5 12h14" />
+    </svg>
+  );
+}
