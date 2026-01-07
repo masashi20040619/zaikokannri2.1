74392812f47d9396e185be2b1b63ba0151fdcce9
fukku0619 <2323066@asia-u.ac.jp>
2026-01-07 14:46:57 +0900

    Add TrashIcon component with SVG rendering

---
+++ src/components/icons/TrashIcon.tsx
@@ -0,0 +1,9 @@
+export default function TrashIcon() {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" className="w-5 h-5" stroke="currentColor" strokeWidth="2">
+      <path d="M3 6h18" />
+      <path d="M8 6V4h8v2" />
+      <path d="M6 6l1 16h10l1-16" />
+    </svg>
+  );
+}
