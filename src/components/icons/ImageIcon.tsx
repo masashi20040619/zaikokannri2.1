f1dde37a4ae920556fdd7b6259b06cd71e9b33ba
fukku0619 <2323066@asia-u.ac.jp>
2026-01-07 14:49:18 +0900

    Create ImageIcon.tsx

---
+++ src/components/icons/ImageIcon.tsx
@@ -0,0 +1,9 @@
+export default function ImageIcon() {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" className="w-10 h-10" stroke="currentColor" strokeWidth="2">
+      <path d="M21 19V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2z" />
+      <path d="M8 11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+      <path d="M21 15l-5-5L5 21" />
+    </svg>
+  );
+}
